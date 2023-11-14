318a16870da8d490ccb14d5a96a3e281345e4ac8
Wendy Nguyen <wn73@drexel.edu>
2023-11-14 02:12:32 +0000

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,7 +4,19 @@ import NavBar from '../components/NavBar';
 function About() {
     return (
         <div className='about' id='about'>
-            <h1>About</h1>
+            <h1 style = "font-family:Trebuchet MS; color:#FFC600">About</h1>
+
+            <h2 style="font-family:Trebuchet MS; color:MidnightBlue;">Who we are</h2>
+                <p style = "font-family:Trebuchet MS;">
+                DragonNest allows landlords to create accounts and post their houses for rent details, including housing conditions, fees and contact information, following our required template to ensure the reliability.
+                </p>
+
+            <h2 style="font-family:Trebuchet MS;color:MidnightBlue;">What we do</h2>  
+            // are those bold words alright???
+                <p style = "font-family:Trebuchet MS;"> 
+                
+                </p>
+            
         </div>
     );
 }
