5d2e5f369559538673fad07e23cfaee2de21ae07
duong <ducduongto@n3-65-240.dhcp.drexel.edu>
2023-11-30 16:11:57 -0500

    Hi

--- src/components/Listing.js
+++ src/components/Listing.js
@@ -2,7 +2,7 @@ import React from 'react';
 // Import any additional assets or styles here
 
 // The Listing component now accepts additional props for the host's name and bed type.
-function Listing({ imageUrl, hostName, description, bedType, onListingClick }) {
+function Listing({ imageUrl, hostName, description, bedType, onListingClick, title }) {
   // Function to handle when a listing is clicked
   const handleClick = () => {
     onListingClick();
@@ -13,6 +13,7 @@ function Listing({ imageUrl, hostName, description, bedType, onListingClick }) {
     <div className="listing" onClick={handleClick}>
       <img src={imageUrl} alt={`Stay at ${hostName}`} className="listing-image" />
       <div className="listing-details">
+        <h2 className="listing-title">{title}</h2>
         <h2 className="listing-host-name">{hostName}</h2>
         <p className="listing-description">{description}</p>
         <p className="listing-bed-type">{bedType}</p>
--- src/pages/ListingPage.js
+++ src/pages/ListingPage.js
@@ -5,6 +5,7 @@ import Listing from '../components/Listing';
 import room1 from '../assets/room1.jpeg';
 import room2 from '../assets/room2.jpeg';
 import room3 from '../assets/room3.jpeg';
+import room4 from '../assets/room4.jpeg';
 import Footer from '../components/Footer';
 import '../components/Style.css'; 
 
@@ -12,7 +13,8 @@ const mockListings = [
   {
     id: 1,
     imageUrl: room1,
-    hostName: 'Stay with Laura',
+    hostName: 'Hung',
+    title: 'Hung Phang',
     description: 'Enjoy a bright and comfortable studio in downtown.',
     bedType: 'Queen bed',
     price: '$670 month'
@@ -21,7 +23,8 @@ const mockListings = [
   {
     id: 2,
     imageUrl: room2,
-    hostName: 'Stay with Alex',
+    hostName: 'Huy',
+    title: 'Duc Dang',
     description: 'Spacious two-bedroom with great city views.',
     bedType: 'Double bed',
     price: '$788 month'
@@ -30,10 +33,22 @@ const mockListings = [
   {
     id: 3,
     imageUrl: room3,
-    hostName: 'Stay with Roy',
+    hostName: 'Superman',
+    title: 'Superman',
     description: 'Spacious two-bedroom with great city views.',
     bedType: 'Two double beds',
     price: '$788 month'
+    // ... other listing properties
+  },
+
+  {
+    id: 4,
+    imageUrl: room4,
+    hostName: 'Wendy',
+    title: 'Wendy Nguyen',
+    description: 'Two Comfortable and cosy bedrooms; Near the Center City',
+    bedType: 'Queen bed',
+    price: '$800 month'
     // ... other listing properties
   },
   // ... more mock listings
@@ -57,7 +72,7 @@ function ListingPage() {
             key={listing.id}
             imageUrl={listing.imageUrl}
             title={`Stay with ${listing.hostName}`} 
-            hostName={listing.hostName}
+            hostName={`Host: ${listing.hostName}`}
             description={listing.description}
             bedType={listing.bedType}
             price={listing.price}
