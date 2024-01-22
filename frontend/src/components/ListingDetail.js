38e26704e8ea15872beabc97fd6e94b2312e1c9b
duong <ducduongto@duongs-mbp.mynetworksettings.com>
2024-01-21 23:20:34 -0500

    test

--- frontend/src/components/ListingDetail.js
+++ frontend/src/components/ListingDetail.js
@@ -27,25 +27,6 @@ function ListingDetail({ imageUrl, hostName, description, bedType, price, room, 
             {/* Add more details here if needed */}
         </div>
     </div>
-
-    //     <div>
-    //   <h2>{detailData.title}</h2> {/* Display the title of the listing */}
-    //   <p>{detailData.description}</p> {/* Display the description of the listing */}
-      
-    //   {/* Display other relevant details of the listing */}
-    //   <img src={detailData.imageUrl} alt={detailData.title} />
-    //   <span>Price: {detailData.price}</span>
-    //   <span>Bedrooms: {detailData.bedrooms}</span>
-    //   <span>Bathrooms: {detailData.bathrooms}</span>
-    //   <span>Pets Allowed: {detailData.petsAllowed ? 'Yes' : 'No'}</span>
-      
-    //   {/* Contact information section */}
-    //   <div>
-    //     <h3>Contact Information:</h3>
-    //     <p>{detailData.contactName}</p>
-    //     <p>{detailData.contactEmail}</p>
-    //     {/* If available, add more contact details like phone number here */}
-    //   </div>
   );
 }
 
