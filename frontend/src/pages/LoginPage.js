4f8fb795fa84ae680f6e3ef52e1dcb89b271c78e
hp533 <hp533@cs.drexel.edu>
2024-01-21 21:41:29 -0500

    Fixed the toggle left-right

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,45 +1,56 @@
+import React, { useState } from 'react';
 import '../components/Login.css'; 
 
-function LoginPage() {
-    // document.addEventListener('DOMContentLoaded', function () {
-    //     const container = document.getElementById('container');
-    //     const registerBtn = document.getElementById('register');
-    //     const loginBtn = document.getElementById('login');
-    
-    //     registerBtn.addEventListener('click', () => {
-    //         container.classList.add("active");
-    //     });
-    
-    //     loginBtn.addEventListener('click', () => {
-    //         container.classList.remove("active");
-    //     });
-    // });
-    return(
-        <div class="container" id="container">
-            <div class="form-container sign-up">
+
+const LoginPage = () => {
+    const [isActive, setIsActive] = useState(false);
+
+    const handleToggle = () => {
+        setIsActive(!isActive);
+    };
+
+    return (
+        <div className={`container ${isActive ? 'active' : ''}`} id="container">
+            <div className="form-container sign-up">
                 <form>
                     <h1>Create Account</h1>
-                    {/* <div class="social-icons">
-                        <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
-                        <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
-                        <a href="#" class="icon"><i class="fa-brands fa-github"></i></a>
-                        <a href="#" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
+                    {/* <div className="social-icons">
+                        <a href="#" className="icon">
+                        <i className="fa-brands fa-google-plus-g"></i>
+                        </a>
+                        <a href="#" className="icon">
+                        <i className="fa-brands fa-facebook-f"></i>
+                        </a>
+                        <a href="#" className="icon">
+                        <i className="fa-brands fa-github"></i>
+                        </a>
+                        <a href="#" className="icon">
+                        <i className="fa-brands fa-linkedin-in"></i>
+                        </a>
                     </div> */}
-                    <span>or use your email for registeration</span>
+                    <span>or use your email for registration</span>
                     <input type="text" placeholder="Name" />
                     <input type="email" placeholder="Email" />
                     <input type="password" placeholder="Password" />
-                    <button>Sign up</button>
+                    <button>Sign Up</button>
                 </form>
             </div>
-            <div class="form-container sign-in">
+            <div className="form-container sign-in">
                 <form>
                     <h1>Sign In</h1>
-                    {/* <div class="social-icons">
-                        <a href="#" class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
-                        <a href="#" class="icon"><i class="fa-brands fa-facebook-f"></i></a>
-                        <a href="#" class="icon"><i class="fa-brands fa-github"></i></a>
-                        <a href="#" class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
+                    {/* <div className="social-icons">
+                        <a href="#!" className="icon">
+                        <i className="fa-brands fa-google-plus-g"></i>
+                        </a>
+                        <a href="#" className="icon">
+                        <i className="fa-brands fa-facebook-f"></i>
+                        </a>
+                        <a href="#" className="icon">
+                        <i className="fa-brands fa-github"></i>
+                        </a>
+                        <a href="#" className="icon">
+                        <i className="fa-brands fa-linkedin-in"></i>
+                        </a>
                     </div> */}
                     <span>or use your email password</span>
                     <input type="email" placeholder="Email" />
@@ -48,24 +59,26 @@ function LoginPage() {
                     <button>Sign In</button>
                 </form>
             </div>
-            <div class="toggle-container">
-                <div class="toggle">
-                    <div class="toggle-panel toggle-left">
+            <div className="toggle-container">
+                <div className="toggle">
+                    <div className="toggle-panel toggle-left">
                         <h1>Welcome Back!</h1>
-                        <p>Enter your personal details to use all of site features</p>
-                        <button class="hidden" id="login">Sign In</button>
+                        <p>Enter your personal details to use all site features</p>
+                        <button className="hidden" onClick={handleToggle} id="login">
+                        Sign In
+                        </button>
                     </div>
-                    <div class="toggle-panel toggle-right">
+                    <div className="toggle-panel toggle-right">
                         <h1>Hello, Friend!</h1>
-                        <p>Register with your personal details by clicking button below</p>
-                        <button class="hidden" id="register">Sign Up</button>
+                        <p>Register with your personal details to use all site features</p>
+                        <button className="hidden" onClick={handleToggle} id="register">
+                        Sign Up
+                        </button>
                     </div>
                 </div>
             </div>
         </div>
     );
-}
-
-
+};
 
 export default LoginPage;
