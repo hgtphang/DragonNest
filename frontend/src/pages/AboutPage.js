25a97777a7ee3346a233f468b9b2e1c8e1d6cd3c
wn73 <vythao.37cdc@gmail.com>
2024-03-03 20:15:16 -0500

    Update About page UI

--- frontend/src/pages/AboutPage.js
+++ frontend/src/pages/AboutPage.js
@@ -1,14 +1,15 @@
 function AboutPage() {
     return (
         <div className="about-page"> {/* className included from origin/main */}
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             <h1>ABOUT</h1> {/* Keep this heading from HEAD */}
+            <br></br>
             <h2>Who we are</h2>
             <p> DragonNest was established in Spring, 2024 by a group of 4 Drexel College of Computing and Informatics 
                 students, with a view to creating a student-friendly, transparent, interactive platform for Drexel students 
                 to find housings for rent and roommates for unit sharing in the area of Philadelphia. </p>
 
-            {/* Optional: <br></br> - Include this if you need a line break */}
-
+            <br></br>
             <h2>What we do</h2>
             <p> DragonNest allows landlords to create accounts and post their houses for rent details, including housing 
                 conditions, fees and contact information, following our required template to ensure the reliability.</p>
@@ -21,8 +22,13 @@ function AboutPage() {
                 housing’s status, comments on posts will pop up while they are accessing the web and appear in their 
                 notification section.</p>
 
+            <br></br>
             <h2>Follow us</h2>
-            <p>[IG Icon] @dragonnest.philly</p>
+            {/* <a href="#" class="fa fa-instagram"><p>@dragonnest.philly</p></a> */}
+            <a href="#" class="instagram-link">
+                <i class="fa fa-instagram"></i>
+                <p>@dragonnest.philly</p>
+            </a>
         </div>
     );
 }
